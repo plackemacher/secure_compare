852d0ed43038d61a00718ab79c3499b844109899
Patrick Lackemacher <patrick@lackemacher.com>
2017-01-24 09:07:11 -0500

    Add ex_doc package to enable publishing

--- mix.exs
+++ mix.exs
@@ -34,7 +34,9 @@ defmodule SecureCompare.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 
   defp package do
