d2e64fd99ada6de6b22e6a381da7c8d43e4ad5f2
Pat Lackemacher <patrick@lackemacher.com>
2017-08-15 15:36:30 -0400

    Update version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SecureCompare.Mixfile do
   def project do
     [
       app: :secure_compare,
-      version: "0.0.2",
+      version: "0.1.0",
       elixir: "~> 1.3",
       name: "secure_compare",
       source_url: "git@github.com:plackemacher/secure_compare.git",
